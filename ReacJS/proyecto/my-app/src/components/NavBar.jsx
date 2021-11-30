ad080675f75824e54c894c56260779fc0ca4a847
julieta sangari <juuli.sangari@gmail.com>
2021-11-30 00:11:05 -0300

    nav

--- ReacJS/proyecto/my-app/src/components/NavBar.jsx
+++ ReacJS/proyecto/my-app/src/components/NavBar.jsx
@@ -1,17 +1,22 @@
 
 const Navbar = () =>{
     return (
-      
-      <nav className="navbar navbar-light bg-light">
-      <div className="container-fluid">
-        <p className="navbar-brand">Navbar</p>
-        <form className="d-flex">
-          {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"><input/> */}
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-    
+        <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Valkiria</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link" href="#">contacto</a>
+              <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link disabled">Disabled</a>
+            </div>
+          </div>
+        </div>
+      </nav>
     );
 }
   
