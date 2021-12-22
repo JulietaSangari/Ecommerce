2328d35fbc90b68fbdc26ffe508b373afcc3761c
julieta sangari <juuli.sangari@gmail.com>
2021-12-22 20:25:39 -0300

    Routes

--- ReacJS/proyecto/my-app/src/App.js
+++ ReacJS/proyecto/my-app/src/App.js
@@ -2,17 +2,29 @@ import Navbar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer"
 import IteamCount from "./components/ItemCount";
 import {BrowserRouter, Routes, Route} from "react"
+import Home from "./components/home";
+import Detalle from "./components/Detalle";
+import Category from "./components/Category";
+import Checkout from "./components/Checkout";
+import Cart from "./components/Cart";
+import Error from "./components/Error";
 
 export default function app(){
   return(
     <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/Category" element={<ItemListContainer/>}/>
-        <Route exact path="/Item" element={<IteamCount button={0}/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Cart" element={<Cart/>}/>
+        <Route path="/***" element={<Error/>}/>
+        <Route path="/dettalle/:id" element={<Detalle/>}/>
+        <Route path="/category/:category" element={<Category/>}/>
+        <Route path="/checkout/:idTicket" element={<Checkout/>}/>
+        <Route path="/Item" element={<IteamCount button={0}/>}/>
       </Routes>
     </BrowserRouter>
+    
   )
+  
 };
 
---
+++ ReacJS/proyecto/my-app/src/components/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./NavBar";
+
+export default function Home(){
+    return(
+        <h1>
+            soy home
+        </h1>
+    )
+}
